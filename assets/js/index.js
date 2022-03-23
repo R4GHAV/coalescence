6c75eedcb877003b452281c9a29fbd410cf12843
Raghav <rgandotra14@gmail.com>
2022-03-23 16:26:49 +0530

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -83,3 +83,34 @@ faqLabel.forEach(item=>item.onclick=()=>{
    
 })
 
+function view_more() {
+  if (document.querySelector(".view_more").innerHTML == "VIEW MORE") {
+    document.querySelector(".view_more").innerHTML = "VIEW LESS"
+    document.querySelector(".out_more").style.display = "block"
+  } else {
+    document.querySelector(".view_more").innerHTML = "VIEW MORE"
+    document.querySelector(".out_more").style.display = "none"
+  }
+
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
